326fe0f6ec345588211377cd15ea92cf97418ead
oumaima <oumaimaba797@gmail.com>
2025-02-21 20:13:01 +0100

    deleted folder from alx_fe_reactjs

--- alx-react-app-props/src/UserContext.js
+++
@@ -1,6 +0,0 @@
-// src/UserContext.js
-import { createContext } from "react";
-
-const UserContext = createContext(null);  // Correct context initialization
-
-export default UserContext;  // Exporting the context so it can be used in other components
--- alx-react-app-props/src/components/ProfilePage.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/ProfilePage.jsx
-import UserInfo from "./UserInfo";
-
-function ProfilePage() {
-  return <UserInfo />;  // No need to pass props, just use UserContext
-}
-
-export default ProfilePage;
