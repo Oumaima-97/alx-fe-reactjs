bbaa7cde218ef7273032e4716a9f27177f90d3d8
oumaima <oumaimaba797@gmail.com>
2025-02-27 20:27:37 +0000

    update Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,23 +1,13 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#333', color: '#fff' }}>
-      <ul style={{ listStyleType: 'none', display: 'flex' }}>
-        <li style={{ marginRight: '20px' }}>
-          <Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>Home</Link>
-        </li>
-        <li style={{ marginRight: '20px' }}>
-          <Link to="/about" style={{ color: '#fff', textDecoration: 'none' }}>About</Link>
-        </li>
-        <li style={{ marginRight: '20px' }}>
-          <Link to="/services" style={{ color: '#fff', textDecoration: 'none' }}>Services</Link>
-        </li>
-        <li>
-          <Link to="/contact" style={{ color: '#fff', textDecoration: 'none' }}>Contact</Link>
-        </li>
-      </ul>
+    <nav style={{ display: 'flex', justifyContent: 'space-between', padding: '10px' }}>
+      <Link to="/" style={{ textDecoration: 'none' }}>Home</Link>
+      <Link to="/about" style={{ textDecoration: 'none' }}>About</Link>
+      <Link to="/services" style={{ textDecoration: 'none' }}>Services</Link>
+      <Link to="/contact" style={{ textDecoration: 'none' }}>Contact</Link>
     </nav>
   );
 }
