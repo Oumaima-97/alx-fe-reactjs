2c3bdaf3cc9e20bb5c2fffbb069071fdb1a465c7
oumaima <oumaimaba797@gmail.com>
2025-03-11 22:39:43 +0000

    Update tailwaid file

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,10 +1,14 @@
+// tailwind.config.js
+
 module.exports = {
     content: [
-      "./src/**/*.{html,js,jsx,ts,tsx}",
+      "./src/**/*.{js,jsx,ts,tsx}",
+      "./public/index.html"
     ],
+    darkMode: "media", // or "class" if you want to toggle it manually
     theme: {
       extend: {},
     },
     plugins: [],
-  };
+  }
   
