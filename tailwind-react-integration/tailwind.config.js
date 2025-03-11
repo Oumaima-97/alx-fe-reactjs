8dae5f261e00b9ba746edd5f64b3b592f32e8e3e
oumaima <oumaimaba797@gmail.com>
2025-03-11 22:40:43 +0000

    update again

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -2,13 +2,13 @@
 
 module.exports = {
     content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-      "./public/index.html"
+      "./src/**/*.{js,jsx,ts,tsx}",  // Paths to your template files
+      "./public/index.html",         // Include HTML files
     ],
-    darkMode: "media", // or "class" if you want to toggle it manually
+    darkMode: "media", // Use 'media' for system preference or 'class' for manual control
     theme: {
-      extend: {},
+      extend: {},  // Extend the default theme if necessary
     },
-    plugins: [],
+    plugins: [],  // Add Tailwind CSS plugins if needed
   }
   
