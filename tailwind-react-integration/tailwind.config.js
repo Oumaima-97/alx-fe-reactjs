83978d28131419ba33de350974c6cdb5f329482c
oumaima <oumaimaba797@gmail.com>
2025-03-12 17:18:33 +0000

    update for seconde

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,11 +1,7 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Tailwind will scan these files for classes
-    "./public/index.html",        // Include index.html if you have classes there
-  ],
-  darkMode: 'media',  // Optional, defaults to 'media'
-  theme: {
-    extend: {},         // Extend the theme if needed
-  },
-  plugins: [],         // Add plugins if necessary
-}
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: { extend: {} },
+  variants: { extend: {} },
+  plugins: [],
+};
