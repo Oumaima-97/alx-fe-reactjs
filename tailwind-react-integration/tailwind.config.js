da79ea3ded8561c3d1f6b9583fc60d3fe798262b
oumaima <oumaimaba797@gmail.com>
2025-03-12 17:08:56 +0000

    update

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  darkMode: 'media', // Explicitly set dark mode to media query
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensures Tailwind scans all JS, JSX, TS, and TSX files in the src directory
+    "./public/index.html",        // If you're using any Tailwind classes in public/index.html, include it
   ],
   theme: {
     extend: {},
