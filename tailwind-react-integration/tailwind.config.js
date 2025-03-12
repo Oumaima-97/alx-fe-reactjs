fedd83d58441632315d9cc80aa7484fddcb1639e
oumaima <oumaimaba797@gmail.com>
2025-03-12 17:31:29 +0000

    Update fil again

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: ["./src/**/*.{html,js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
   theme: { extend: {} },
   variants: { extend: {} },
