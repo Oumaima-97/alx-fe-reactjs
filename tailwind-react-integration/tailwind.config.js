7b7808631b43570df3b15f842a36586d483a1916
oumaima <oumaimaba797@gmail.com>
2025-03-12 16:58:26 +0000

    Add files

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: 'media', // Explicitly set dark mode to media query
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
