ef0db6d8359f9cac6af9acaed07fb72df18f4d06
oumaima <oumaimaba797@gmail.com>
2025-03-17 22:51:29 +0000

    Add All

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,8 +1,14 @@
+import HomePage from "./components/HomePage";
+
 function App() {
   return (
-    <div className="text-center text-2xl text-blue-500">
-      Tailwind CSS is working! 🎉
+    <div className="min-h-screen bg-gray-100 p-5">
+      <div className="text-center text-2xl text-blue-500 mb-6">
+        Tailwind CSS is working! 🎉
+      </div>
+      <HomePage />
     </div>
   );
 }
+
 export default App;
