db66765a9833d9d69492b165b95d2eaaa09e6cf8
oumaima <oumaimaba797@gmail.com>
2025-03-26 22:58:11 +0000

    update file again

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,7 +3,15 @@ import axios from 'axios';
 
 const BASE_URL = 'https://api.github.com/search/users';
 
-export const searchUsers = async (query, page = 1) => {
+export const searchUsers = async (username, location, minRepos, page = 1) => {
+  let query = username;
+  if (location) {
+    query += ` location:${location}`;
+  }
+  if (minRepos) {
+    query += ` repos:>${minRepos}`;
+  }
+
   try {
     const response = await axios.get(`${BASE_URL}?q=${encodeURIComponent(query)}&page=${page}`);
     return response.data;
